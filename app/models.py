00428cc2ba3f69ac1689830e96f0756b3514bdec
johnliaogithub <johnliao5@gmail.com>
2018-09-04 10:30:48 -0400

    a

--- app/models.py
+++ app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class AddingBooksOrDVDs(models.Model):
+    Author=models.CharField(max_length=200, default="author")
+    Title=models.CharField(max_length=35, default="title")
+    Date=models.CharField(max_length=10, default="dd-mm-yyyy")
+    Picture=models.TextField(default="picture")
